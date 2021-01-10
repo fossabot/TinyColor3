c1cbc815cb87db8d52abdb0cbb98f693a2f3b0f5
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 14:14:15 -0500

    Update TinyColor.js

--- src/TinyColor.js
+++ src/TinyColor.js
@@ -392,58 +392,6 @@ function convertDecimalToHex(d) {
 function convertHexToDecimal(h) {
 	return (parseIntFromHex(h) / 255);
 }
-function stringInputToObject(color) {
-	color
-		.replace(trimLeft,'')
-		.replace(trimRight, '')
-		.toLowerCase();
-	let match;
-	else if (color == 'transparent')
-		return { r: 0, g: 0, b: 0, a: 0, format: "name" };
-	if ((match = matchers.rgb.exec(color)))
-		return { r: match[1], g: match[2], b: match[3] };
-	if ((match = matchers.rgba.exec(color)))
-		return { r: match[1], g: match[2], b: match[3], a: match[4] };
-	if ((match = matchers.hsl.exec(color)))
-		return { h: match[1], s: match[2], l: match[3] };
-	if ((match = matchers.hsla.exec(color)))
-		return { h: match[1], s: match[2], l: match[3], a: match[4] };
-	if ((match = matchers.hsv.exec(color)))
-		return { h: match[1], s: match[2], v: match[3] };
-	if ((match = matchers.hsva.exec(color))) 
-		return { h: match[1], s: match[2], v: match[3], a: match[4] };
-	if ((match = matchers.hex8.exec(color)))
-		return {
-			r: parseIntFromHex(match[1]),
-			g: parseIntFromHex(match[2]),
-			b: parseIntFromHex(match[3]),
-			a: convertHexToDecimal(match[4]),
-			format: named ? "name" : "hex8"
-		};
-	if ((match = matchers.hex6.exec(color)))
-		return {
-			r: parseIntFromHex(match[1]),
-			g: parseIntFromHex(match[2]),
-			b: parseIntFromHex(match[3]),
-			format: named ? "name" : "hex"
-		};
-	if ((match = matchers.hex4.exec(color)))
-		return {
-			r: parseIntFromHex(match[1] + '' + match[1]),
-			g: parseIntFromHex(match[2] + '' + match[2]),
-			b: parseIntFromHex(match[3] + '' + match[3]),
-			a: convertHexToDecimal(match[4] + '' + match[4]),
-			format: named ? "name" : "hex8"
-		};
-	if ((match = matchers.hex3.exec(color)))
-		return {
-			r: parseIntFromHex(match[1] + '' + match[1]),
-			g: parseIntFromHex(match[2] + '' + match[2]),
-			b: parseIntFromHex(match[3] + '' + match[3]),
-			format: named ? "name" : "hex"
-		};
-	return false;
-}
 function inputToRGB(color) {
 	let rgb = { r: 0, g: 0, b: 0 },
 		a = 1,
