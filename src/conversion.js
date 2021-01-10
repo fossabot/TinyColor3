9489a0e901fd9c140bf2aeac53384ef1ea30e4d5
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 16:46:06 -0500

    Update conversion.js

--- src/conversion.js
+++ src/conversion.js
@@ -41,59 +41,64 @@ exports.rgbToHsl = function (r, g, b) {
 	r = bound01(r, 255);
 	g = bound01(g, 255);
 	b = bound01(b, 255);
-	let max = Math.max(r, g, b), min = Math.min(r, g, b),
+	let max = Math.max(r, g, b),
+	    min = Math.min(r, g, b),
 	    h, s, l = (max + min) / 2;
-	if(max == min)
-		h = s = 0; // achromatic
-	else {
-		let d = max - min;
-		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-		switch(max) {
-			case r:
-				h = (g - b) / d + (g < b ? 6 : 0);
-				break;
-			case g:
-				h = (b - r) / d + 2; 
-				break;
-			case b:
-				h = (r - g) / d + 4; 
-				break;
-		}
-		h /= 6;
-	}
+    if(max == min)
+	    h = s = 0;
+    else {
+        let d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max) {
+		case r: 
+			h = (g - b) / d + (g < b ? 6 : 0); 
+			break;
+            case g: 
+			h = (b - r) / d + 2; 
+			break;
+            case b: 
+			h = (r - g) / d + 4; 
+			break;
+        }
 
-	return { h: h, s: s, l: l };
+        h /= 6;
+    }
+
+    return { h: h, s: s, l: l };
 }
 
 exports.hslToRgb = function (h, s, l) {
-	let r, g, b, q, p;
-	h = bound01(h, 360);
-	s = bound01(s, 100);
-	l = bound01(l, 100);
+    let r, g, b;
 
-	function hue2rgb(p, q, t) {
-		if(t < 0)
-			t += 1;
-		if(t > 1)
-			t -= 1;
-		if(t < 1/6)
-			return p + (q - p) * 6 * t;
-		if(t < 1/2)
-			return q;
-		if(t < 2/3)
-			return p + (q - p) * (2/3 - t) * 6;
-		return p;
-	}
+    h = bound01(h, 360);
+    s = bound01(s, 100);
+    l = bound01(l, 100);
 
-	if(s === 0)
-		r = g = b = l; // achromatic
-	else
-		q = l < 0.5 ? l * (1 + s) : l + s - l * s,
-			p = 2 * l - q,
-			r = hue2rgb(p, q, h + 1/3),
-			g = hue2rgb(p, q, h),
-			b = hue2rgb(p, q, h - 1/3);
-	return { r: r * 255, g: g * 255, b: b * 255 };
+    function hue2rgb(p, q, t) {
+        if(t < 0) 
+		t += 1;
+        if(t > 1) 
+		t -= 1;
+        if(t < 1/6) 
+		return p + (q - p) * 6 * t;
+        if(t < 1/2) 
+		return q;
+        if(t < 2/3) 
+		return p + (q - p) * (2/3 - t) * 6;
+        return p;
+    }
+
+    if(s === 0)
+	    r = g = b = l; // achromatic
+    else {
+        let q = l < 0.5 ? l * (1 + s) : l + s - l * s,
+	    p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3),
+		g = hue2rgb(p, q, h),
+		b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return { r: r * 255, g: g * 255, b: b * 255 };
 }
 
 exports.rgbToHsv = function (r, g, b) {
