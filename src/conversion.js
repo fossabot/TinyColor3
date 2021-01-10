38b738cdb0a340fa9d903fb15fcc7a6758159ac4
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 15:06:15 -0500

    Update conversion.js

--- src/conversion.js
+++ src/conversion.js
@@ -1,6 +1,9 @@
 function isOnePointZero(n) {
     return typeof n == "string" && n.indexOf('.') != -1 && parseFloat(n) === 1;
 }
+function isPercentage(n) {
+    return typeof n === "string" && n.indexOf('%') != -1;
+}
 function convertDecimalToHex(d) {
 	return Math.round(parseFloat(d) * 255).toString(16);
 }
@@ -11,7 +14,7 @@ function bound01(n, max) {
 	if (isOnePointZero(n)) 
 		n = "100%";
 	let processPercent = isPercentage(n);
-	n = mathMin(max, Math.max(0, parseFloat(n)));
+	n = Math.min(max, Math.max(0, parseFloat(n)));
 	if (processPercent)
 		n = parseInt(n * max, 10) / 100;
 	if ((Math.abs(n - max) < 0.000001))
