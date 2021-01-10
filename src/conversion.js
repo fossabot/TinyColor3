65d89112308a8e12a34ac759944d0dae3042f16a
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2021-01-10 16:49:33 -0500

    Update conversion.js

--- src/conversion.js
+++ src/conversion.js
@@ -44,27 +44,25 @@ exports.rgbToHsl = function (r, g, b) {
 	let max = Math.max(r, g, b),
 	    min = Math.min(r, g, b),
 	    h, s, l = (max + min) / 2;
-    if(max == min)
-	    h = s = 0;
-    else {
-        let d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch(max) {
-		case r: 
-			h = (g - b) / d + (g < b ? 6 : 0); 
-			break;
-            case g: 
-			h = (b - r) / d + 2; 
-			break;
-            case b: 
-			h = (r - g) / d + 4; 
-			break;
-        }
-
-        h /= 6;
-    }
-
-    return { h: h, s: s, l: l };
+	if(max == min)
+		h = s = 0;
+	else {
+		let d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+		switch(max) {
+			case r: 
+				h = (g - b) / d + (g < b ? 6 : 0); 
+				break;
+			case g: 
+				h = (b - r) / d + 2; 
+				break;
+			case b:
+				h = (r - g) / d + 4; 
+				break;
+		}
+		h /= 6;
+	}
+	return { h: h, s: s, l: l };
 }
 
 exports.hslToRgb = function (h, s, l) {
@@ -89,7 +87,7 @@ exports.hslToRgb = function (h, s, l) {
     }
 
     if(s === 0)
-	    r = g = b = l; // achromatic
+	    r = g = b = l;
     else {
         let q = l < 0.5 ? l * (1 + s) : l + s - l * s,
 	    p = 2 * l - q;
